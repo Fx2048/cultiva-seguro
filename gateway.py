6fd245a0bd3dc4b5615b93d12e6c57d63cc56c4d
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2026-07-02 15:26:02 -0500

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -1,28 +1,29 @@
 import requests
 
-URL = "https://sjxaexssraavijbysqsd.supabase.co/functions/v1/sensor-data"
+url = "https://sjxaexssraavijbysqsd.supabase.co/functions/v1/sensor-data"
 
-HEADERS = {
-    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeGFleHNzcmFhdmlqYnlzcXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0ODYyMjgsImV4cCI6MjA5MDA2MjIyOH0.Rvact_njjT4wNWnSBKuFI_5saHMiQrDnUUi9r9GMmKA",
-    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeGFleHNzcmFhdmlqYnlzcXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0ODYyMjgsImV4cCI6MjA5MDA2MjIyOH0.Rvact_njjT4wNWnSBKuFI_5saHMiQrDnUUi9r9GMmKA",
-    "Content-Type": "application/json"
+headers = {
+    "apikey":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeGFleHNzcmFhdmlqYnlzcXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0ODYyMjgsImV4cCI6MjA5MDA2MjIyOH0.Rvact_njjT4wNWnSBKuFI_5saHMiQrDnUUi9r9GMmKA",
+    "Authorization":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeGFleHNzcmFhdmlqYnlzcXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0ODYyMjgsImV4cCI6MjA5MDA2MjIyOH0.Rvact_njjT4wNWnSBKuFI_5saHMiQrDnUUi9r9GMmKA",
+    "Content-Type":"application/json"
 }
 
 data = {
-    "lat": -13.52,
-    "lon": -71.97,
-    "temperatura": 8.6,
-    "humedad": 81,
-    "humedad_suelo": 54,
-    "device_id": "WILLAY001",
-    "source": "raspberry"
+
+    "lat":-13.52,
+    "lon":-71.97,
+
+    "temperatura":11.8,
+    "humedad":82,
+
+    "humedad_suelo":46,
+
+    "device_id":"EMI001",
+
+    "source":"raspberry"
+
 }
 
-r = requests.post(
-    URL,
-    json=data,
-    headers=HEADERS
-)
+r=requests.post(url,json=data,headers=headers)
 
-print(r.status_code)
 print(r.text)
