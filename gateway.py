cd7740a1a4d2d0192b268d6aab35b10306942391
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2026-07-04 08:02:38 -0500

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -1,30 +1,14 @@
-import requests
+import serial
 
-SUPABASE_URL = "https://sjxaexssraavijbysqsd.supabase.co/functions/v1/sensor-data"
+# Abrir el puerto serial
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeGFleHNzcmFhdmlqYnlzcXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0ODYyMjgsImV4cCI6MjA5MDA2MjIyOH0.Rvact_njjT4wNWnSBKuFI_5saHMiQrDnUUi9r9GMmKA"
+print("Gateway iniciado...")
+print("Esperando datos...\n")
 
-headers = {
-    "apikey": API_KEY,
-    "Authorization": f"Bearer {API_KEY}",
-    "Content-Type": "application/json"
-}
+while True:
+    if ser.in_waiting:
+        linea = ser.readline().decode('utf-8').strip()
 
-payload = {
-    "lat": -13.1631,
-    "lon": -72.5450,
-    "temperatura": 12.5,
-    "humedad": 80,
-    "humedad_suelo": 45,
-    "device_id": "EMI001",
-    "source": "raspberry"
-}
-
-r = requests.post(
-    SUPABASE_URL,
-    headers=headers,
-    json=payload
-)
-
-print(r.status_code)
-print(r.text)
+        if linea:
+            print(linea)
